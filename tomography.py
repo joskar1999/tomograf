e32b008b2186f26bef05869ff5788a7bd77e5740
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-03-22 19:06:03 +0100

    Added dcm reading function

--- tomography.py
+++ tomography.py
@@ -1,19 +1,17 @@
 import cv2
 import numpy as np
-# from skimage import iradon
 from PIL import Image
-from scipy import signal
+import pydicom
+from pydicom.pixel_data_handlers import gdcm_handler, pillow_handler
 
 lines = []
 sinogram = []
 
 
 def radon_transform(image_name, number_of_emitters=180, angular_step=1, angular_range=180):
-    image = cv2.imread(image_name, 0)
-
+    # image = cv2.imread(image_name, 0)
+    image = create_pixel_array_from_dicom(image_name)
     image = pad_image(image)
-    print(image.shape)
-
     ANGLES = np.arange(0, angular_range, angular_step)
     OFFSETS = np.linspace(-image.shape[0] / 2, image.shape[0] / 2, number_of_emitters)
     radius = image.shape[0] / 2
@@ -24,27 +22,19 @@ def radon_transform(image_name, number_of_emitters=180, angular_step=1, angular_
         lines_row = []
         for offset in OFFSETS:
             positions = get_positions(radius, angle, offset)
-            single_line = create_line(radius, positions)
+            single_line = create_line(positions)
             color = calculate_single_line(image, single_line)
             sinogram_row.append(color)
             lines_row.append(single_line)
         sinogram.append(sinogram_row)
         lines.append(lines_row)
 
-    # print(lines)
-    # print(sinogram)
-
     image = Image.fromarray(np.array(sinogram))
     image.show()
-    # cv2.imwrite("RadonSheppchuj.jpg", np.array(sinogram))
+
     return np.array(sinogram)
 
-    # cv2.imshow('tomograf', image)
 
-    # cv2.waitKey(0)
-
-
-# funkcja wyrownujaca prostokatne zdjecie do kwadratowego
 def pad_image(image):
     width, height = np.shape(image)
 
@@ -65,7 +55,6 @@ def pad_image(image):
     return image
 
 
-# pobieranie pozycji
 def get_positions(radius, angle, offset):
     x1, y1 = radius * np.cos(angle), radius * np.sin(angle)
     x2, y2 = -x1, -y1
@@ -85,8 +74,7 @@ def calculate_single_line(image, single_line):
     return np.array(result).mean() if len(result) > 0 else 0
 
 
-# Algorytm bresenhama do tworzenia linii
-def create_line(radius, points):
+def create_line(points):
     x1, y1, x2, y2 = int(points[0]), int(points[1]), int(points[2]), int(points[3])
     result = []
 
@@ -116,19 +104,14 @@ def create_line(radius, points):
                 e += dy
             result.append((x1, y1))
 
-    radius_pow_2 = radius ** 2
-    return [(x, y) for x, y in result if ((x - radius) ** 2 + (y - radius) ** 2) <= radius_pow_2]
+    return [(x, y) for x, y in result]
 
 
-# odtworzenie linii
 def reverse(input_image, output_image_name="output_image.png"):
-    input_image = cv2.imread(input_image, 0)
-
+    input_image = create_pixel_array_from_dicom(input_image)
     sinogram_reverse = np.array(sinogram)
-
     output_image = np.zeros((input_image.shape[1], input_image.shape[0]))
-
-    sinogram_reverse = filter_projection(sinogram_reverse)
+    sinogram_reverse = filter_sinogram(sinogram_reverse)
     for i in range(sinogram_reverse.shape[0]):
         for j in range(sinogram_reverse.shape[1]):
             for x, y in lines[i][j]:
@@ -138,6 +121,14 @@ def reverse(input_image, output_image_name="output_image.png"):
     image = Image.fromarray(np.array(output_image))
     image.show()
     cv2.imwrite(output_image_name, output_image)
+
+
+def hyperbolic_filter(number_freq):
+    filtering_array = []
+    for i in range(1, number_freq + 2):
+        filtering_array.append(1 / i + 0.01)
+    filtering_array = np.concatenate((filtering_array[number_freq - 1:0:-1], filtering_array), axis=0)
+    return filtering_array / max(filtering_array)
 
 
 def low_pass_filter(number_freq, value):
@@ -150,17 +141,24 @@ def hann_filter(number_freq):
     return filtering_array
 
 
-def ramp_filter(number_freq):
+def triangular_filter(number_freq):
     filtering_array = 2 * np.arange(number_freq + 1) / np.float32(2 * number_freq)
     filtering_array = np.concatenate((filtering_array, filtering_array[number_freq - 1:0:-1]), axis=0)
     return filtering_array
 
 
-def filter_projection(sinogram):
+def cropped_triangular_filter(number_freq):
+    filtering_array = 2 * np.arange(number_freq + 1) / np.float32(2 * number_freq)
+    filtering_array[0:int(number_freq / 40)] = 0
+    filtering_array = np.concatenate((filtering_array, filtering_array[number_freq - 1:0:-1]), axis=0)
+    return filtering_array
+
+
+def filter_sinogram(sinogram):
     number_angles, number_offsets = sinogram.shape
     number_freq = 2 * int(2 ** (int(np.ceil(np.log2(number_offsets)))))
 
-    filter_array = low_pass_filter(number_freq, 0.02)
+    filter_array = triangular_filter(number_freq)
 
     padded_sinogram = np.concatenate((sinogram, np.zeros((number_angles, 2 * number_freq - number_offsets))), axis=1)
 
@@ -170,6 +168,24 @@ def filter_projection(sinogram):
     return padded_sinogram[:, :number_offsets]
 
 
+def create_pixel_array_from_dicom(image_name):
+    if image_name.endswith('.dcm'):
+
+        image_dcm = pydicom.dcmread(image_name)
+        img = Image.fromarray(np.uint8(image_dcm.pixel_array[0] * 255))
+        # img.show()
+        return np.array(img)
+
+    return cv2.imread(image_name)
+
+
+def read_dicom(image_name):
+    if image_name.endswith('.dcm'):
+        return pydicom.dcmread(image_name)
+    return None
+
+
 if __name__ == '__main__':
-    radon_transform("Shepp_logan.png", 180, 1, 180)
-    reverse("Shepp_logan.png")
+    pydicom.config.image_handlers = [gdcm_handler, pillow_handler]
+    radon_transform("0002.dcm", 180, 1, 180)
+    reverse("0002.dcm")
